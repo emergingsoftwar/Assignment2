172f53c09a8c2060b3b07d64e66768edc1875b63
Terry147 <54358027+Terry147@users.noreply.github.com>
2021-03-05 21:01:36 -0500

    Adding Js Jest

---
+++ test/gilded_rose.test.js
@@ -0,0 +1,88 @@
+const {Shop, Item} = require("../src/gilded_rose");
+
+describe("First Test Suite", function() {
+  
+  it("sellin value and quality test", function() {
+    const gildedRose = new Shop([new Item("foo", 2, 2)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(1);
+    expect(items[0].quality).toBe(1)
+  });
+
+  it("Once the sell by date has passed, Quality degrades twice as fast", function() {
+    const gildedRose = new Shop([new Item("Past sell date", 0, 40)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(38);
+  });
+
+  it("The Quality of an item is never negative", function() {
+    const gildedRose = new Shop([new Item("Never negative", 5, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  });
+
+  it("Aged Brie actually increases in Quality the older it gets", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", 2, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(1);
+    expect(items[0].sellIn).toBe(1);
+  });
+  
+
+
+});
+
+describe("Quality negative", function() {
+  it("Quality negative", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", -1, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(12);
+  });
+});
+
+describe("The Quality of an item is never more than 50", function() {
+  it("Quality", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", 5, 52)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(50);
+  });
+});
+
+describe("Backstage passes, like aged brie, increases in Quality as its SellIn value approaches", function() {
+  it("Backstage pass test", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 5, 40)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(43);
+  });
+
+  it("Backstage pass test 2", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 10, 40)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(42);
+  });
+ 
+  it("Backstage pass test 3", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 40)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  });
+
+});
+
+describe("Sulfuras", function() {
+  it("being a legendary item, never has to be sold or decreases in Quality", function() {
+    const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 0, 30)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(30);
+    expect(items[0].sellIn).toBe(0);
+  });
+});
+
+describe("Sellin", function() {
+  it("Less than 0", function() {
+    const gildedRose = new Shop([new Item("Bread", -1, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(8);
+
+  });
+});
